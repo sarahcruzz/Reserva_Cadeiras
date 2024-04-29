6530f4c805430f1753c8a09b1cea1bb6387190b6
sarahcruzz <sarah.cruz0492@gmail.com>
2024-04-29 19:08:45 -0300

    finalizado

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import os
-
+ 
 cadeiras = {}
 qtd_cadeiras = 10
-
+ 
 for i in range(qtd_cadeiras):
         cadeiras[f'B{i + 1}'] = f"[B {i + 1}]"
         print(f" [B {i + 1}]", end=' ')
-        
+       
 print("\n" , "-" * 70)
-
+ 
 while True:
     reserva = input("Reservar a cadeira: ").upper()
-
+ 
     try:
         if cadeiras[reserva] == '[ --- ]':
-            raise ValueError('ERRO: Lugar ocupado!')
+            print('ERRO: Lugar ocupado!')
         else:
             print(f"CADEIRA {cadeiras[reserva]} RESERVADA!")
             cadeiras.update({reserva:'[ --- ]'})
     except KeyError:
         print("ERRO: Cadeira não existe!")
-        
+       
     simnao = input("\nDeseja reservar outra cadeira? (s/n) ")
     os.system('cls' if os.name == 'nt' else 'clear')  
-        
+       
     if simnao.lower() != 's':
         print("As cadeiras reservadas foram as seguintes: ")
         for i in cadeiras.values():
@@ -34,6 +34,5 @@ while True:
             print(i, end=' ')
         print("\n" , "-" * 65)
         continue
-        
-    
-        
+       
+   
